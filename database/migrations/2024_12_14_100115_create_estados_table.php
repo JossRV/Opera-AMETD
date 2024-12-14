62d7ad264af39d2f0f0a1a635d148fa8ba510295
JossRV <jossrv74@gmail.com>
2024-12-14 10:07:59 -0600

    migraciones de login y registro

--- database/migrations/2024_12_14_100115_create_estados_table.php
+++ database/migrations/2024_12_14_100115_create_estados_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cat_estados', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('respass');
-            $table->unsignedBigInteger('persona_id');
-            $table->rememberToken();
+            $table->string('estado');
+            $table->string('abreviacion');
+            $table->text('descripcion');
+            $table->smallInteger('estatus');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cat_estados');
     }
 };
