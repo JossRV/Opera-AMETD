6610bfce98dc606248ecbe9e1c052092dbc5fcee
joseamadoferrer <developerjoseamadoferrer@hotmail.com>
2024-12-17 11:35:20 -0600

    corrrecion alertas perfil

--- resources/views/modules/personas/perfil.blade.php
+++ resources/views/modules/personas/perfil.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
+
+@if (session('success'))
+    <div class="alert alert-success">
+        <strong>{{ session('success') }}</strong>
+    </div>
+@endif
+@if (session('error'))
+    <div class="alert alert-danger">
+        <strong>{{ session('error') }}</strong>
+    </div>
+@endif
+@if ($errors->all())
+    <div class="alert alert-danger">
+        <p>Porfavor verifica los mensajes de error</p>
+        <p>{{ $errors->first('password') }}</p>
+    </div>
+@endif
+
+
 @section('content')
     {{-- personalizacion de perfil --}}
     <div class="container-fluid">
